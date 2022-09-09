68654ace84f7a4398fb6bdfc34c410d278358a36
Gayoung Song <98731537+gayoung106@users.noreply.github.com>
2022-09-09 19:10:42 +0900

    theme color css

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import useLocalStorage from "use-local-storage";
 import "./index.css";
 
 function App() {
+  const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light");
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
   return (
-    <div className="app">
+    <div className="app" data-theme={theme}>
       <div className="login">
-        <hi>Login</hi>
+        <h1>Login</h1>
         <div className="container">
-          <div className="top">
-            <i class="fab fa-google"></i>
-            <i class="fab fa-facebook-square"></i>
-            <i class="fab fa-linkedin"></i>
-            <i class="fab fa-twitter-square"></i>
-            <i class="fab fa-apple"></i>
-          </div>
-          <p className="divider">
-            <span>Or</span>
-          </p>
           <form>
             <label>E-mail</label>
             <input type="email" placeholder="enter your Email" />
@@ -34,10 +31,21 @@ function App() {
             <a href="/">Reset Password</a>
           </div>
           <p className="create">Create Account</p>
+
+          <p className="divider">
+            <span>Or</span>
+          </p>
+          <div className="top">
+            <i class="fab fa-google"></i>
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fab fa-apple"></i>
+          </div>
         </div>
         <div className="theme-toggle">
           <h2>Light Theme</h2>
-          <i className="fas fa-toggle-on"></i>
+          <i onClick={switchTheme} className="fas fa-toggle-on"></i>
         </div>
       </div>
     </div>
